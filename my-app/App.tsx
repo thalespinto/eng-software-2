c86ed6f147e9586706822a7a9e9cf2fe20e82d1a
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-04-25 16:02:23 -0300

    feature: bottom nav bar

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import BottomNav from "./components/BottomNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomNav />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ my-app/screens/HItchHike/index..tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const HitchHike = () => {
+  return (
+    <>
+      <View>
+        <Text>DESENVOLVER PEDIR CARONA AQUI</Text>
+      </View>
+    </>
+  );
+};
+
+export default HitchHike;
