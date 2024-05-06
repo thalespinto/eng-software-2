08d6c81148232709b5d11bd4c6c04c7b96cf7142
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-05-06 18:56:17 -0300

    feature: user and login interfaces

---
+++ my-app/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  createdAt: Date;
+  updatedAt: Date;
+  id: number;
+  login: string;
+  cpf: string;
+  senha: string;
+  nome: string;
+  esta_oferecendo_carona: boolean;
+  reputacao?: number;
+}
