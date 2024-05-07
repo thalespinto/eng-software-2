36cf376c350bd61f936a3dc4443c6aef52271c58
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-05-07 19:25:41 -0300

    feature: component map

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -4,13 +4,16 @@ import { theme } from "./styles/theme";
 import Routes from "./Routes";
 import AuthProvider from "./Providers/AuthProvider";
 import UserProvider from "./Providers/UserProvider";
+import LocationProvider from "./Providers/LocationProvider";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <UserProvider>
         <AuthProvider>
-          <Routes />
+          <LocationProvider>
+            <Routes />
+          </LocationProvider>
         </AuthProvider>
       </UserProvider>
     </ThemeProvider>
