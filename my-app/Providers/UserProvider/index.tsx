6422d1d52df677fe8787434664d89e845ed45915
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-05-09 20:02:36 -0300

    feature: mocked user

--- my-app/Providers/UserProvider/index.tsx
+++ my-app/Providers/UserProvider/index.tsx
@@ -8,7 +8,17 @@ export const userContext = createContext<{
 } | null>(null);
 
 const UserProvider = ({ children }: { children: any }) => {
-  const [user, setUser] = useState<IUser | undefined>();
+  const [user, setUser] = useState<IUser | undefined>({
+    createdAt: new Date("2024-05-06T08:00:00Z"),
+    updatedAt: new Date("2024-05-06T08:30:00Z"),
+    id: 1,
+    login: "usuario123",
+    cpf: "123.456.789-00",
+    senha: "senha123",
+    nome: "João da Silva",
+    esta_oferecendo_carona: true,
+    reputacao: 4.5,
+  });
 
   return (
     <userContext.Provider value={{ user: user, setUser: setUser }}>
